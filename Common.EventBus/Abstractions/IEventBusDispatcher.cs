683d21376d54c62936933f01f66ee4ff9e1ed87e
Amir Shokri <Shokri.Amir@Okala.Com>
2023-02-26 09:00:10 +0330

    add event bus with abstractions

---
+++ Common.EventBus/Abstractions/IEventBusDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using Common.EventBus.Events;
+
+namespace Common.EventBus.Abstractions;
+
+public interface IEventBusDispatcher
+{
+    Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : IntegrationEvent;
+        
+    Task PublishAsync<T>(T message, string topic, CancellationToken cancellationToken = default)
+        where T : IntegrationEvent;
+}
+
---
+++ Common.EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Common.EventBus.Events;
+
+public class IntegrationEvent
+{
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedOn = DateTime.Now;
+    }
+
+    public IntegrationEvent(Guid id , DateTime createdOn)
+    {
+        Id = id;
+        CreatedOn = createdOn;
+    }
+    
+    [JsonPropertyName("id")]
+    public Guid Id { get; private set; }
+
+    [JsonPropertyName("createdOn")]
+    public DateTime CreatedOn { get; private set; }
+}
